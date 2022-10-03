01d13a0f7cefa0f7507108da59c9abc04551e1b3
Jaromir Stanko <stanko.jaromir@gmail.com>
2022-10-03 15:46:55 +0200

    added dellay after clearing pending bits

--- Src/main.c
+++ Src/main.c
@@ -158,8 +158,10 @@ void EXTI3_IRQHandler(void)
 	}
 
 	//Clear pending register flag
-	NVIC->ICPR[0] |= (1 << 9);
 	EXTI->PR |= (EXTI_PR_PIF3);
+	NVIC->ICPR[0] |= (0x0200);
+
+	for(int cnt = 0; cnt < 500; ++cnt){};
 }
 
 /* USER CODE BEGIN 4 */
